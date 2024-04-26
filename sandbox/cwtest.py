fcf2d380b0032e1c5e848330a5ea262ef59df560
KenenthAtchon <kenneth.atchon@hotmail.com>
2024-04-25 23:03:03 -0500

    remove stuff

--- sandbox/cwtest.py
+++
@@ -1,9 +0,0 @@
-from pycrossword import Crossword
-
-crossword = Crossword(width=15, height=15)
-crossword.add_word("python", 1, 1, "horizontal")
-crossword.add_word("javascript", 5, 3, "vertical")
-crossword.generate()
-crossword.display()
-# or
-crossword.save("my_crossword.png")
